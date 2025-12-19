6fc9f7038628644e1c6987f02a02d0bdb4b6e294
bala-amypo <bala@amypo.in>
2025-12-19 09:18:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/LoanRequestRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.LoanRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface LoanRequestRepository extends JpaRepository<LoanRequest, Long> {
+    List<LoanRequest> findByUserId(Long userId);
+    List<LoanRequest> findByStatus(String status);
+}
