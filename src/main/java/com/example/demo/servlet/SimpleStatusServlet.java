9b57f985f82f849bdf7a543ef5e8c36d97d9dcb4
bala-amypo <bala@amypo.in>
2025-12-22 07:58:40 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -8,6 +8,6 @@ public class SimpleStatusServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write("SimpleStatusServlet is running");
+        resp.getWriter().write("ok");
     }
 }
