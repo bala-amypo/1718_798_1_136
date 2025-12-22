ed1fb1178b7b636400c5a5ed40bcd20411f1e950
bala-amypo <bala@amypo.in>
2025-12-22 04:43:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/LoanDtos.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+public class LoanDtos {
+    private Long id;
+    private Double amount;
+    private String purpose;
+    private String status;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public String getPurpose() { return purpose; }
+    public void setPurpose(String purpose) { this.purpose = purpose; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
