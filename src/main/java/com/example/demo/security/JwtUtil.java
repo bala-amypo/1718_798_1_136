d5e7ad4ad99db38f3372400f3d9f8c06977c9cfd
bala-amypo <bala@amypo.in>
2025-12-22 09:16:36 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,14 +1,29 @@
 package com.example.demo.security;
 
-import java.util.HashMap;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtUtil {
 
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
     public String generateToken(Map<String, Object> claims, String subject) {
-        return subject + "-token";
-    }
-    public String generateToken(String subject) {
-        return generateToken(new HashMap<>(), subject);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
     }
 }
